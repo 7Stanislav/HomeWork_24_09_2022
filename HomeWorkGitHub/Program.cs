aa9f321349fda55431ab937a963f669f08276afc
«User» <9323354@gmail.com>
2022-09-24 11:24:55 +0300

    Completed task 1

--- HomeWorkGitHub/Program.cs
+++ HomeWorkGitHub/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число");
+int number = int.Parse(Console.ReadLine());
+if ((number > 99999) || (number < 10000))
+{
+    Console.WriteLine("Число должно быть пятизначным");
+}
+else
+{
+
+    int num1 = (number / 10000);
+    int num2 = (number % 10000 / 1000);
+    int num3 = (number % 1000 / 100);
+    int num4 = (number % 100 / 10);
+    int num5 = (number % 10);
+    
+    if ((num1 == num5) && (num2 == num4))
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
